ed966ca0fa6193cd928c777d55ef707a7e1f1636
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-18 12:34:43 +0530

    chore: Initial commit - boilerplate code

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! This is an HTTP server built using Node.js & Express.');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
+});
